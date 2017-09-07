e4264e158a4bf0bb857ad5af1bb5533d2b7bf91f
andrew <andrew8412@gmail.com>
2017-09-07 19:00:59 -0400

    added board

--- galactic-struggle/src/App.js
+++ galactic-struggle/src/App.js
@@ -8,11 +8,21 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Galactic Struggle!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="board">
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+        </div>
+
+        <p className="asdf">/Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates amet laborum, id iure quasi consequatur dolor, ex fugiat sed, odio blanditiis et sunt eum dolorem totam eligendi iste eveniet commodi!</p>
+        
+      
       </div>
     );
   }
