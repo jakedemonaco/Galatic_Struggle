b540022b95c6f807f0f1d293b29280e40d0f0e94
Jake DeMonaco <jakedemonaco@MacBook-Pro.local>
2017-09-09 14:04:37 -0400

    update movement.js

--- galactic-struggle/src/Components/movement.js
+++ galactic-struggle/src/Components/movement.js
@@ -3,19 +3,19 @@ var Deck = require("./deck.js");
 var Index = 0||1||2||3||4||5||6||7||8||9||10||11||12||13||14||15||16||17||18||19||20||21||22||23||24||25||26||27||28||29||30||31||32||33||34||35||36||37||38||39||40||41||42||43||44||45||46||47;
 var Card = Deck[Index];
 
-var Player1Array1 = [undefined, undefined, undefined, undefined, undefined, undefined];
-var Player1Array2 = [undefined, undefined, undefined, undefined, undefined, undefined];
-var Player1Array3 = [undefined, undefined, undefined, undefined, undefined, undefined];
-var Player1Array4 = [undefined, undefined, undefined, undefined, undefined, undefined];
+var Player1Array1 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var Player1Array2 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var Player1Array3 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var Player1Array4 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
 
-var Player2Array1 = [undefined, undefined, undefined, undefined, undefined, undefined];
-var Player2Array2 = [undefined, undefined, undefined, undefined, undefined, undefined];
-var Player2Array3 = [undefined, undefined, undefined, undefined, undefined, undefined];
-var Player2Array4 = [undefined, undefined, undefined, undefined, undefined, undefined];
+var Player2Array1 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var Player2Array2 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var Player2Array3 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var Player2Array4 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
 
-var BoardArray1 = [undefined, undefined, undefined, undefined, undefined, undefined];
-var BoardArray2 = [undefined, undefined, undefined, undefined, undefined, undefined];
-var BoardArray3 = [undefined, undefined, undefined, undefined, undefined, undefined];
-var BoardArray4 = [undefined, undefined, undefined, undefined, undefined, undefined];
+var BoardArray1 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var BoardArray2 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var BoardArray3 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var BoardArray4 = [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
 
 
