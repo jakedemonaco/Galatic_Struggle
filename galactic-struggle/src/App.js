3392465aa9f46410ef681080c296af1fc025ed9e
andrew <andrew8412@gmail.com>
2017-09-09 13:25:40 -0400

    red sqaure works

--- galactic-struggle/src/App.js
+++ galactic-struggle/src/App.js
@@ -2,28 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: "RED",
+      boardcolor: "WHITE"
+    }
+
+    this.colors = {
+      "RED": {
+        "hexColor": '#f00'
+      },
+      "WHITE": {
+        "hexColor": '#fff'
+      }
+    }
+  }
+
   render() {
+
+    const squareStyle = {
+      "backgroundColor": typeof this.colors[this.state.color] === 'undefined' ? '#fff' : this.colors[this.state.color].hexColor
+    };
+
+    const boardStyle = {
+      "backgroundColor": typeof this.colors[this.state.color] === 'undefined' ? '#fff' : this.colors[this.state.boardcolor].hexColor
+    }
+
+
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Galactic Struggle!</h2>
+          <h2>Galactic Struggle</h2>
         </div>
 
         <div className="row">
 
           <div className="col-lg-2 playerOneHand">
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
+            <div className="square" onClick = {() => this.setState({color: "WHITE", boardcolor:"RED" })} style={squareStyle}>{this.state.color}</div>
           </div> 
+          
+          
           
           <div className="col-lg-7">
             <div className="row board-one">
-              <div className="square"></div>
+              <div className="square square-id-1" style={boardStyle} ></div>
               <div className="square"></div>
               <div className="square"></div>
               <div className="square"></div>
--- galactic-struggle/src/Components/board.js
+++ galactic-struggle/src/Components/board.js
@@ -1,39 +1,39 @@
-/**
- * This class allows you to create a Game Board with a configurable, 
- * underlying grid. 
- * 
- * You'll want to figure out what kinds of details go into updateBoard (!)
- */
-class GameBoard {
-  constructor (defaultWidth = 4, defaultHeight = 6) {
-    // 1. Create grid
-    this.grid = []
-    for (let i = 0; i < defaultHeight; i += 1) {
-      this.grid.push(Array(defaultWidth).fill())
-    }
+// /**
+//  * This class allows you to create a Game Board with a configurable, 
+//  * underlying grid. 
+//  * 
+//  * You'll want to figure out what kinds of details go into updateBoard (!)
+//  */
+// class GameBoard {
+//   constructor (defaultWidth = 4, defaultHeight = 6) {
+//     // 1. Create grid
+//     this.grid = []
+//     for (let i = 0; i < defaultHeight; i += 1) {
+//       this.grid.push(Array(defaultWidth).fill())
+//     }
 
-    // 2. Create array to save previous states
-    this.prevStates = []
-  }
-  displayBoard = () => {
-    console.log(JSON.stringify(this.grid, null, 2))
-  }
-  // TODO: Fill out this method!
-  updateBoard = (playerOneCard, playerTwoCard) => {
-    // 1. Create a copy of this.grid
+//     // 2. Create array to save previous states
+//     this.prevStates = []
+//   }
+//   displayBoard = () => {
+//     console.log(JSON.stringify(this.grid, null, 2))
+//   }
+//   // TODO: Fill out this method!
+//   updateBoard = (playerOneCard, playerTwoCard) => {
+//     // 1. Create a copy of this.grid
 
-    // 2a. Update the COPY of the grid with the new cards, etc.
+//     // 2a. Update the COPY of the grid with the new cards, etc.
 
-    // 2b. Update any other state: Update player energies
+//     // 2b. Update any other state: Update player energies
 
-    // 3. Once you're sure your updated copy is final...
-      // 3a. ...Save the current grid into your list of past states: 
-      this.prevStates.push(this.state)
-      // 3b. ...And then set this.grid = copiedGrid
+//     // 3. Once you're sure your updated copy is final...
+//       // 3a. ...Save the current grid into your list of past states: 
+//       this.prevStates.push(this.state)
+//       // 3b. ...And then set this.grid = copiedGrid
   
-    // X. For debugging purposes
-    this.displayBoard()
-  }
-}
+//     // X. For debugging purposes
+//     this.displayBoard()
+//   }
+// }
 
-module.exports = GameBoard
+// module.exports = GameBoard
